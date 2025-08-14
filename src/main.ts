960d525218b0528312184c3592a3f70eb9d4d226
JustinDo2909 <hieudeptraivodichh@gmail.com>
2025-08-14 14:21:15 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express'; // ✅ default import, không dùng namespace
+
+const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.BASEURL ?? 3000);
+  try {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+    await app.init(); // Không dùng app.listen ở Vercel
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.error(e.message);
+    } else {
+      console.error(String(e));
+    }
+  }
 }
+
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
+
+export default server;
